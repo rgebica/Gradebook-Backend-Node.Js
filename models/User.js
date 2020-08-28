a8e43b7ed559dec343f938be3d4926963a6ae0ad
Rafał <rgebica@gmail.com>
2020-08-28 18:18:50 +0200

    Added database connection, user register and logout

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    first_name: {
+        type: String
+    },
+    last_name: {
+        type: String
+    },
+    birth_date: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = User = mongoose.model('users', UserSchema);
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+const mongoose = require('mongoose');
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+
+const mongoURI = 'mongodb://localhost:27017/gradebook';
+
+mongoose
+    .connect(
+        mongoURI,
+        { useNewUrlParser: true }
+    )
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+const Users = require('./routes/Users');
+
+app.use('/users', Users)
+
+app.listen(port, function() {
+    console.log('Server is running on port: ' + port)
+});
